1878a0c6b092bca48faba43c36f3b9d6f99bb6b1
Dave Hoffman <spaghettidave80@gmail.com>
2017-12-15 14:50:53 -0500

    Added bcrypt to hash paswords before storing them in the DB

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 var UserSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -26,6 +27,17 @@ var UserSchema = new mongoose.Schema({
     required: true
   }
 });
-
+UserSchema.pre('save', function(next){
+  var user = this;
+  bcrypt.genSalt(10, function(err, salt){
+    bcrypt.hash(user.password, salt, function(err, hash){
+      if(err){
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
+});
 var User = mongoose.model('User', UserSchema);
 module.exports = User;
